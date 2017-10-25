00de9d845560bb82f1c1f6d67c10d668d5e01461
charge <ahujaarush@gmail.com>
2017-10-25 11:05:18 +0530

    question cascading

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from database import Base
 class SolvedQuestion(Base):
         __tablename__ = 'solved_questions'
         username = Column(String(50), ForeignKey('hackers.username'), primary_key=True)
-        question_id = Column(Integer, ForeignKey('questions.id'), primary_key=True)
+        question_id = Column(Integer, ForeignKey('questions.id'), primary_key=True, ondelete="CASCADE")
         date = Column(DateTime, default=datetime.datetime.utcnow)
         question = relationship("Question", cascade="all,delete")
 
