b92c763bab646f50a73e744c99bc88a1e4e9450a
charge <ahujaarush@gmail.com>
2017-10-26 12:34:48 +0530

    added service worker

--- static/js/main.js
+++ static/js/main.js
@@ -24,3 +24,15 @@ function checkFlag() {
     }
   });
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
