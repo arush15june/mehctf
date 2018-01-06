7472dbadf0d8c406a63dc03a032cff124c61efae
charge <ahujaarush@gmail.com>
2018-01-07 01:24:42 +0530

    fixing sot algo

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from operator import attrgetter
 
 def sortScoreDict(unsorted):
-    return OrderedDict(reversed(sorted(unsorted.items(), key=lambda t: (t[1]['score'], t[1]['last_question_date']))))
+    return OrderedDict(sorted(unsorted.items(), key=lambda t: (t[1]['score'], t[1]['last_question_date'])))
 
 def sortSolvedQuesList(unsorted):
     return sorted(unsorted, key=lambda solvedq: solvedq.date)
