c7c59cf73269b73d2363c3feba049ffbb16ff5f4
charge <ahujaarush@gmail.com>
2018-01-07 00:54:10 +0530

    fixed sorting algorithm

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from operator import attrgetter
 
 def sortScoreDict(unsorted):
-    return OrderedDict(sorted(unsorted.items(), key=lambda t: (t[1]['score'], t[1]['last_question_date']) , reverse=True))
+    return OrderedDict(sorted(unsorted.items(), key=lambda t: (t[1]['last_question_date'], t[1]['score']) , reverse=True))
 
 def sortSolvedQuesList(unsorted):
     return sorted(unsorted, key=lambda solvedq: solvedq.date)
