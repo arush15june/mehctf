3cb10021b9c0460e268e6b7847c3a0103a5b4256
charge <ahujaarush@gmail.com>
2018-01-07 01:33:31 +0530

    fixing sorting algo

--- helpers.py
+++ helpers.py
@@ -2,7 +2,9 @@ from collections import OrderedDict
 from operator import attrgetter
 
 def sortScoreDict(unsorted):
-    return OrderedDict(sorted(unsorted.items(), key=lambda t: (t[1]['score'], t[1]['last_question_date'])))
+    sorted_list = sorted(unsorted.items(), key=lambda t: (t[1]['last_question_date']) )
+    sorted_list = sorted(sorted_list, key=lambda t: (t[1]['score']), reverse=True)
+    return OrderedDict(sorted_list)
 
 def sortSolvedQuesList(unsorted):
     return sorted(unsorted, key=lambda solvedq: solvedq.date)
